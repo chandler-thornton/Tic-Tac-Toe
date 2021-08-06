1d35ba74fcdfe421cc352227192511ff54f9348a
chandler-thornton <chanman1218@outlook.com>
2021-08-05 23:17:09 -0400

    Added functionality to checkExistsWinner methoda

--- TIcTacToe/src/Board.java
+++ TIcTacToe/src/Board.java
@@ -7,8 +7,19 @@ public class Board {
 	private static char[][] board = {{' ',' ',' '},
 								     {' ',' ',' '},
 								     {' ',' ',' '}};
+
+	private static int[][] boardLayout = {{0,0,0},
+								          {0,0,0},
+		                                  {0,0,0}};
+	
+	private static int[] rowScore = {0,0,0};
+	private static int[] columnScore = {0,0,0};
+	private static int[] diagonalScore = {0,0};
+
+	
 	private static boolean existsWinner = false;
 	private static int winner;
+	private static int winnerNum = 0;
 	private static int compType;
 	
 	//Start game
@@ -65,30 +76,54 @@ public class Board {
 	    	switch(pos) {
 	    		case 1:
 	    			board[0][0] = 'X';
+	    			columnScore[0] += 1;
+	    			rowScore[0] += 1;
+	    			diagonalScore[0] += 1;
 	    			break;
 	    		case 2:
 	    			board[0][1] = 'X';
+	    			columnScore[1] += 1;
+	    			rowScore[0] += 1;
 	    			break;
 	    		case 3:
 	    			board[0][2] = 'X';
+	    			columnScore[2] += 1;
+	    			rowScore[0] += 1;
+	    			diagonalScore[1] += 1;
 	    			break;
 	    		case 4:
 	    			board[1][0] = 'X';
+	    			columnScore[0] += 1;
+	    			rowScore[1] += 1;
 	    			break;
 	    		case 5:
 	    			board[1][1] = 'X';
+	    			columnScore[1] += 1;
+	    			rowScore[1] += 1;
+	    			diagonalScore[0] += 1;
+	    			diagonalScore[1] += 1;
 	    			break;
 	    		case 6:
 	    			board[1][2] = 'X';
+	    			columnScore[2] += 1;
+	    			rowScore[1] += 1;
 	    			break;
 	    		case 7:
 	    			board[2][0] = 'X';
+	    			columnScore[0] += 1;
+	    			rowScore[2] += 1;
+	    			diagonalScore[1] += 1;
 	    			break;
 	    		case 8:
 	    			board[2][1] = 'X';
+	    			columnScore[1] += 1;
+	    			rowScore[2] += 1;
 	    			break;
 	    		case 9 :
 	    			board[2][2] = 'X';
+	    			columnScore[2] += 1;
+	    			rowScore[2] += 1;
+	    			diagonalScore[0] += 1;
 	    			break;
 	    	}
 	    }
@@ -96,37 +131,88 @@ public class Board {
 	    	switch(pos) {
 	    		case 1:
 	    			board[0][0] = 'O';
+	    			columnScore[0] -= 1;
+	    			rowScore[0] -= 1;
+	    			diagonalScore[0] -= 1;
 	    			break;
 	    		case 2:
 	    			board[0][1] = 'O';
+	    			columnScore[1] -= 1;
+	    			rowScore[0] -= 1;
 	    			break;
 	    		case 3:
 	    			board[0][2] = 'O';
+	    			columnScore[2] -= 1;
+	    			rowScore[0] -= 1;
+	    			diagonalScore[1] -= 1;
 	    			break;
 	    		case 4:
 	    			board[1][0] = 'O';
+	    			columnScore[0] -= 1;
+	    			rowScore[1] -= 1;
 	    			break;
 	    		case 5:
 	    			board[1][1] = 'O';
+	    			columnScore[1] -= 1;
+	    			rowScore[1] -= 1;
+	    			diagonalScore[0] -= 1;
+	    			diagonalScore[1] -= 1;
 	    			break;
 	    		case 6:
 	    			board[1][2] = 'O';
+	    			columnScore[2] -= 1;
+	    			rowScore[1] -= 1;
 	    			break;
 	    		case 7:
 	    			board[2][0] = 'O';
+	    			columnScore[0] -= 1;
+	    			rowScore[2] -= 1;
+	    			diagonalScore[1] -= 1;
 	    			break;
 	    		case 8:
 	    			board[2][1] = 'O';
+	    			columnScore[1] -= 1;
+	    			rowScore[2] -= 1;
 	    			break;
 	    		case 9 :
 	    			board[2][2] = 'O';
+	    			columnScore[2] -= 1;
+	    			rowScore[2] -= 1;
+	    			diagonalScore[0] -= 1;
 	    			break;
 	    	}
 	    }
 	}
 	
 	public static boolean checkExistsWinner() {
-		return false;
+		for(int r = 0; r < 2; r++) {
+			if(rowScore[r] == 3 || rowScore[r] == -3) {
+				winnerNum = rowScore[r];
+			}
+		}
+		for(int c = 0; c < 2; c++) {
+			if(columnScore[c] == 3 || columnScore[c] == -3) {
+				winnerNum = columnScore[c];
+			}
+		}
+		for(int d = 0; d < 1; d++) {
+			if(diagonalScore[d] == 3 || diagonalScore[d] == -3) {
+				winnerNum = diagonalScore[d];
+			}
+		}
+		if(winnerNum == 3) {
+			winner = 1;
+			existsWinner = true;
+			return true;
+		}
+		else if(winnerNum == -3) {
+			winner = 2;
+			existsWinner = true;
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 }
